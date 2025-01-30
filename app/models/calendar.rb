7825bba303ecb57fb6821412c59a46dce5b9495c
hagiya0121 <koazu0121@gmail.com>
2025-01-30 13:25:07 +0900

    カレンダーの新規作成機能

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Calendar < ApplicationRecord
+  before_validation :set_year
+
   validates :title, :year, presence: true
   validates :year, uniqueness: true
 
   has_many :entries, dependent: :destroy
+
+  private
+
+  def set_year
+    self.year = Time.current.year
+  end
 end
