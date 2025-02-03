d6304f27093ec80da87828d887d51aaf06adc00a
hagiya0121 <koazu0121@gmail.com>
2025-02-03 19:48:27 +0900

    バリデーションのエラーメッセージを日本語化

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
 module FjordCalendar
   class Application < Rails::Application
     config.load_defaults 7.1
+    config.i18n.default_locale = :ja
+    config.active_model.i18n_customize_full_message = true
 
     config.autoload_lib(ignore: %w(assets tasks))
 
