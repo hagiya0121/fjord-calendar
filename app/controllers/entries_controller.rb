0366de83e2e0642e6ee1e8e1ea96a2571a99f7dc
hagiya0121 <koazu0121@gmail.com>
2025-02-12 18:56:28 +0900

    非同期でモーダル画面を開けるように実装

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class EntriesController < ApplicationController
+  before_action :set_calendar, only: %i[new create]
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = @calendar.entries.build(entry_params)
+    @entry.user = User.first
+
+    if @entry.save
+      flash.now.notice = '記事を登録しました'
+      render :create
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_calendar
+    @calendar = Calendar.find(params[:calendar_id])
+  end
+
+  def entry_params
+    params.require(:entry).permit(:title, :url, :registration_date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :calendars
+  resources :calendars do
+    resources :entries, shallow: true
+  end
 end
