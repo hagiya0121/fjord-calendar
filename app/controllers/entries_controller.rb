f03a3c95a3f910ace3f144c739c8200812713e10
hagiya0121 <koazu0121@gmail.com>
2025-02-21 13:16:32 +0900

    カレンダーに登録された記事の編集機能を実装

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,14 +2,19 @@
 
 class EntriesController < ApplicationController
   before_action :set_calendar, only: %i[new create]
+  before_action :set_entry, only: %i[edit update]
 
   def new
-    @entry = Entry.new
+    @entry = @calendar.entries.build
+  end
+
+  def edit
+    @calendar = @entry.calendar
   end
 
   def create
     @entry = @calendar.entries.build(entry_params)
-    @entry.user = User.first
+    @entry.user = User.last
 
     if @entry.save
       flash.now.notice = '記事を登録しました'
@@ -19,10 +24,24 @@ class EntriesController < ApplicationController
     end
   end
 
+  def update
+    @calendar = @entry.calendar
+    if @entry.update(entry_params)
+      flash.now.notice = '記事を更新しました'
+      render :update
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_calendar
     @calendar = Calendar.find(params[:calendar_id])
+  end
+
+  def set_entry
+    @entry = Entry.find(params[:id])
   end
 
   def entry_params
