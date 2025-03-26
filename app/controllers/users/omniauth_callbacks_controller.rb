a95c1d29424375ee6d1d28e90d883b3fa144b0c3
hagiya0121 <koazu0121@gmail.com>
2025-03-26 17:18:20 +0900

    ログイン時のリダイレクト先をログインしたページになるように実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,12 +8,13 @@ module Users
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
+        sign_in @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
       else
         session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url
       end
+
+      redirect_to after_sign_in_path_for(@user)
     end
 
     def failure
