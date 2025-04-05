431f18015df7c0226eab9b71f830dd17b224168b
hagiya0121 <koazu0121@gmail.com>
2025-04-05 10:36:19 +0900

    テストが通るように修正

--- app/javascript/controllers/entry_modal_controller.js
+++ app/javascript/controllers/entry_modal_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  close() {
+    this.element.classList.add("hidden");
+  }
+
   closeOnOutsideClick(event) {
     if (event.target === this.element) {
-      this.element.classList.add("hidden");
+      this.close();
     }
   }
 }
