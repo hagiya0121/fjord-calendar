16f551dbc45833c246474859b80aff64a5f775dc
hagiya0121 <koazu0121@gmail.com>
2025-04-05 10:25:56 +0900

    ドロップダウンメニューの外側をクリックすると消えるように実装

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,10 +1,23 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="dropdown"
 export default class extends Controller {
   static targets = ["menu"];
+
+  connect() {
+    document.addEventListener("click", this.handleClickOutside);
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleClickOutside);
+  }
 
   toggle() {
     this.menuTarget.classList.toggle("hidden");
   }
+
+  handleClickOutside = (event) => {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.classList.add("hidden");
+    }
+  };
 }
--- app/javascript/controllers/entry_modal_controller.js
+++ app/javascript/controllers/entry_modal_controller.js
@@ -1,13 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  close() {
-    this.element.classList.add("hidden");
-  }
-
   closeOnOutsideClick(event) {
     if (event.target === this.element) {
-      this.close();
+      this.element.classList.add("hidden");
     }
   }
 }
