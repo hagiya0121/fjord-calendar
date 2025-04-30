f011f4203a51b6e313088e2d03ff664422da1666
hagiya0121 <koazu0121@gmail.com>
2025-04-30 18:26:59 +0900

    Twitter cardを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,15 +6,33 @@ module ApplicationHelper
       site: 'Fjord Calendar',
       reverse: true,
       charset: 'utf-8',
-      description: 'Fjord Bootcampのアドベントカレンダーを作成・閲覧できるサービスです。',
+      description: 'FjordCalendar はフィヨルドブートキャンプ専用の、アドベントカレンダーアプリです。',
       keywords: 'Fjord Bootcamp, アドベントカレンダー, プログラミング',
       separator: '|',
-      og: {
-        title: :title,
-        description: :description,
-        url: request.original_url,
-        image: image_url('og_image.png')
-      }
+      og: og_tags,
+      twitter: twitter_tags
+    }
+  end
+
+  private
+
+  def og_tags
+    {
+      title: :title,
+      description: :description,
+      url: request.original_url,
+      image: image_url('og.png'),
+      site_name: 'Fjord Calendar'
+    }
+  end
+
+  def twitter_tags
+    {
+      title: :title,
+      description: :description,
+      card: 'summary_large_image',
+      image: image_url('og.png'),
+      url: request.original_url
     }
   end
 end
