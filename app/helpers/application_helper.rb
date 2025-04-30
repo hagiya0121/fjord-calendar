01cb9989e3bc14d38df0b0a86cbf7b5e17b4271a
hagiya0121 <koazu0121@gmail.com>
2025-04-30 17:19:16 +0900

    ogpを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Fjord Calendar',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Fjord Bootcampのアドベントカレンダーを作成・閲覧できるサービスです。',
+      keywords: 'Fjord Bootcamp, アドベントカレンダー, プログラミング',
+      separator: '|',
+      og: {
+        title: :title,
+        description: :description,
+        url: request.original_url,
+        image: image_url('og_image.png')
+      }
+    }
+  end
 end
