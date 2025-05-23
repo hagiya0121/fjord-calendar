e818cd7a88e3cf2bb3831567100d9ca2196df5ee
hagiya0121 <koazu0121@gmail.com>
2025-05-23 19:11:02 +0900

    カレンダーのアイコン表示数を定数化

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module CalendarsHelper
+  MAX_CALENDAR_AVATARS = 11
+
+  def max_calendar_avatars
+    MAX_CALENDAR_AVATARS
+  end
+
   def markdown_to_html(text)
     converted_html = Commonmarker.to_html(text, options: { extension: { header_ids: nil } })
     raw(converted_html) # rubocop:disable Rails/OutputSafety
